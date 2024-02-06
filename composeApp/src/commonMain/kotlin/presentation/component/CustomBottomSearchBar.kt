ca2f29ab4e79a294f0f8c838a9aeeab8a4f8ceaf
samAricha <samaricha001@gmail.com>
2024-02-06 21:48:34 +0300

    complete customization of chat screen

--- composeApp/src/commonMain/kotlin/presentation/component/CustomBottomSearchBar.kt
+++ composeApp/src/commonMain/kotlin/presentation/component/CustomBottomSearchBar.kt
@@ -112,6 +112,7 @@ fun CustomBottomSearchBar(
                     focusedContainerColor = Cream2,
                     unfocusedContainerColor = Cream2,
                     disabledContainerColor = Cream2,
+                    cursorColor = SecondaryLightColor,
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
                 ),
